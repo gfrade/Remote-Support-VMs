77ac24fa419d225eb2dd3e826819e4cf323aa798
gfrade <93204737+gfrade@users.noreply.github.com>
2024-02-02 16:12:54 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Event listener for computer button clicks
+  socket.on("toggleAvailability", (computerId) => {
+    io.emit("updateAvailability", computerId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
